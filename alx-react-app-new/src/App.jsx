8815190b147d192d3fd644287a75b09f3670698f
Jayson-Sey <jaysonsey005@gmail.com>
2025-11-16 18:21:17 +0200

    Apply Inline CSS Styling to React Components

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
-import MainContent from './components/mainContent'
+import MainContent from './components/MainContent'
 import UserProfile from './components/UserProfile'
 
 function App() {
